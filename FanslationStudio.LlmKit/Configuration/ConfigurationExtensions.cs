8af790d262bf83d1ed34de48cfe14a9c683d1c17
Josh Freitas <tabjfr@rwwa.com.au>
2026-08-30 08:03:38 +0800

    First Release

--- FanslationStudio.LlmKit/Configuration/ConfigurationExtensions.cs
+++ FanslationStudio.LlmKit/Configuration/ConfigurationExtensions.cs
@@ -58,7 +58,7 @@ public static class ConfigurationExtensions
             response.Runtime.Models[model.Name] = runtimeConfig;
 
             // Merge custom prompts if they are specified
-            var customPromptsPath = model.CustomPromptsPath == string.Empty ?
+            var customPromptsPath = string.IsNullOrEmpty(model.CustomPromptsPath) ?
                 $"{workingDirectory}/{model.Name}Prompts"
                 : $"{workingDirectory}/{model.CustomPromptsPath}";
 
